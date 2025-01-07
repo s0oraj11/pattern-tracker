27a17171a70082bd0121b6c564317732e48e85d8
suraj <ridesuraj@gmail.com>
2025-01-07 21:34:56 +0530

    initial_commit

---
+++ src/main/java/com/patternsite/pattern_tracker/PatternTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.patternsite.pattern_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patternsite/pattern_tracker/PatternTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patternsite.pattern_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
